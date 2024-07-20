6ec5385dc22ed11fab9b84b1ecdb60b2b258c27b
abdohassan <abdelrahman.hassan@sbs-sa.net>
2024-07-20 21:26:17 +0300

    implement video card list and styling

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,14 +1,79 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { FlatList, Image, RefreshControl, Text, View } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import SearchInput from "../../components/SearchInput";
+import Trending from "../../components/Trending";
+import EmptyState from "../../components/EmptyState";
+import { getAllPosts } from "../../lib/appwrite";
+import useAppWrite from "../../lib/useAppWrite";
+import VideoCard from "../../components/VideoCard";
 
 const Home = () => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  // get all posts
+  const { data: posts, loading, refetch } = useAppWrite(getAllPosts);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    // re-call videos => if any new videos appears
+    await refetch();
+    setRefreshing(false);
+  };
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className="flex my-6 px-4 space-y-6">
+            <View className="flex justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">User</Text>
+              </View>
+
+              {/* Logo */}
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="cover"
+                />
+              </View>
+            </View>
+
+            {/* search in the videos */}
+            <SearchInput placeholder="Search for a video topic" />
+
+            {/* Latest videos */}
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-lg font-pregular text-gray-100 mb-3">
+                Latest Videos
+              </Text>
+
+              {/* Trending videos */}
+              {/* <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} /> */}
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subTitle="Be the first one to upload a video"
+          />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+    </SafeAreaView>
   );
 };
 
 export default Home;
-
-const styles = StyleSheet.create({});
